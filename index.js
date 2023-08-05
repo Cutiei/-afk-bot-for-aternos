b2e062f411b1492e18025941a781ca84d74c51f3
Cutiei <140823642+Cutiei@users.noreply.github.com>
2023-08-05 22:18:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,54 +6,66 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = ['forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
+
+
 var bot = mineflayer.createBot({
   host: host,
-port : data["port"],
-  username: username
+  port: data["port"],
+  username: username + generateRandomString(5)
 });
 function getRandomArbitrary(min, max) {
-       return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 
 }
-bot.on('login',function(){
-	console.log("Logged In")
+bot.on('login', function() {
+  console.log("Logged In")
 });
 bot.on('time', function() {
-    if (connected <1) {
-        return;
-    }
-    if (lasttime<0) {
+  if (connected < 1) {
+    return;
+  }
+  if (lasttime < 0) {
+    lasttime = bot.time.age;
+  } else {
+    var randomadd = Math.random() * maxrandom * 20;
+    var interval = moveinterval * 20 + randomadd;
+    if (bot.time.age - lasttime > interval) {
+      if (moving == 1) {
+        bot.setControlState(lastaction, false);
+        moving = 0;
         lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
-            }
-        }
+      } else {
+        var yaw = Math.random() * pi - (0.5 * pi);
+        var pitch = Math.random() * pi - (0.5 * pi);
+        bot.look(yaw, pitch, false);
+        lastaction = actions[Math.floor(Math.random() * actions.length)];
+        bot.setControlState(lastaction, true);
+        moving = 1;
+        lasttime = bot.time.age;
+        bot.activateItem();
+      }
     }
+  }
 });
 
-bot.on('spawn',function() {
-    connected=1;
+bot.on('spawn', function() {
+  connected = 1;
 });
 
